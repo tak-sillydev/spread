d744bd1f6c4aada1e5988cefb1427a21e00f249d
tak-sillydev <takumi.tsuchiya718@gmail.com>
2023-09-29 23:41:08 +0900

    WritePixel -> PixelWriter (func to class)

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -10,45 +10,72 @@ struct PixelColor {
 	uint8_t	r, g, b;
 };
 
-// 指定位置に点を描画する。
-// 成功 = 0, 失敗 = 0 以外
-int WritePixel(const FrameBufferConfig& conf, int x, int y, const PixelColor& c) {
-	const int	pos = conf.px_per_scanline * y + x;
-	uint8_t		*p;
+class PixelWriter {
+public:
+	PixelWriter(const FrameBufferConfig& conf) : config_(conf) {}
+	virtual ~PixelWriter() = default;
 
-	switch (conf.pixelformat)
-	{
-	case kPixelRGBResv8BitPerColor:
-		p = &conf.framebuf[4 * pos];
+	virtual void Write(int x, int y, const PixelColor& c) = 0;
+
+protected:
+	uint8_t* PixelAt(int x, int y) {
+		return config_.framebuf + 4 * (config_.px_per_scanline * y + x); 
+	}
+
+private:
+	const FrameBufferConfig& config_;
+};
+
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+public:
+	using PixelWriter::PixelWriter;	// 子クラスのコンストラクタが親のものに転送される
+
+	virtual void Write(int x, int y, const PixelColor& c) override {
+		auto p = PixelAt(x, y);
 
 		p[0] = c.r;
 		p[1] = c.g;
 		p[2] = c.b;
-		break;
+	}
+};
 
-	case kPixelBGRResv8BitPerColor:
-		p = &conf.framebuf[4 * pos];
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+public:
+	using PixelWriter::PixelWriter;
+
+	virtual void Write(int x, int y, const PixelColor& c) override {
+		auto p = PixelAt(x, y);
 
 		p[0] = c.b;
 		p[1] = c.g;
 		p[2] = c.r;
-		break;
-	
-	default:
-		return -1;
 	}
-	return 0;
-}
+};
+
+void* operator new(size_t size, void *buf) { return buf; }
+void  operator delete(void* obj) noexcept {}
+
+char pxwriter_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter *pxwriter;
 
 extern "C" void KernelMain(const FrameBufferConfig& fbufconf) {
+	switch (fbufconf.pixelformat) {
+	case kPixelRGBResv8BitPerColor:
+		pxwriter = new (pxwriter_buf) RGBResv8BitPerColorPixelWriter(fbufconf);
+		break;
+
+	case kPixelBGRResv8BitPerColor:
+		pxwriter = new (pxwriter_buf) BGRResv8BitPerColorPixelWriter(fbufconf);
+		break;
+	}
 	for (int y = 0; y < fbufconf.vresolution; y++) {
 		for (int x = 0; x < fbufconf.hresolution; x++) {
-			WritePixel(fbufconf, x, y, { 0xff, 0xff, 0xff });
+			pxwriter->Write(x, y, { 0xff, 0xff, 0xff });
 		}
 	}
 	for (int y = 100; y < 200; y++) {
 		for (int x = 100; x < 200; x++) {
-			WritePixel(fbufconf, x, y, { 0, 0xff, 0 });
+			pxwriter->Write(x, y, { 0, 0xff, 0 });
 		}
 	}
 	while (1) { __asm__("hlt"); }
