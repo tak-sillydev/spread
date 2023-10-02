49db0d22f6de507a481dd0fdb4fa2fa4f7a691d8
tak-sillydev <takumi.tsuchiya718@gmail.com>
2023-10-03 01:28:21 +0900

    Changed type of header files to use symbolic-link in SpreadLoaderPkg

--- SpreadLoaderPkg/framebuffer_config.hpp
+++ SpreadLoaderPkg/framebuffer_config.hpp
@@ -1,16 +1,1 @@
-#pragma once
-
-#include <stdint.h>
-
-enum PixelFormat {
-	kPixelRGBResv8BitPerColor,
-	kPixelBGRResv8BitPerColor,
-};
-
-typedef struct {
-	uint8_t		*framebuf;
-	uint32_t	px_per_scanline;
-	uint32_t	hresolution;
-	uint32_t	vresolution;
-	enum PixelFormat	pixelformat;
-} FrameBufferConfig;
+../kernel/framebuffer_config.hpp
